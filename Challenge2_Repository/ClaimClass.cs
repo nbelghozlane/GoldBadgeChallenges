a9f4e6c2e3a196f89bcf0c2bc2cc98759a2cdb40
Nadia <nbelghoz@gmail.com>
2020-12-08 19:28:41 -0500

    Challenge 2 file setup, added code to ClaimClass

---
+++ Challenge2_Repository/ClaimClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2_Repository
+{
+    public enum ClaimType
+    {
+        Car = 1,
+        Home,
+        Theft
+    }
+    public class ClaimClass
+    {
+        public string ClaimID { get; set; }
+        public ClaimType ClaimType { get; set; }
+        public string Description { get; set; }
+        public string ClaimAmount { get; set; }
+        public string DateOfIncident { get; set; }
+        public string DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+
+        public ClaimClass() { }
+
+        public ClaimClass(string claimID, ClaimType claimType, string description, string claimAmount, string dateOfIncident, string dateOfClaim, bool isValid)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+            IsValid = isValid;
+        }
+    }
+}
---
+++ Challenge2_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge2_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge2_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2e4c4a8-ce66-4b44-aa12-b1f16c9d1ee5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
