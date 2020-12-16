22aca5c93128f8d698c4e02c9c08c1ccff1dcf97
Nadia <nbelghoz@gmail.com>
2020-12-16 09:56:19 -0500

    Completed challenge 1 unit tests

--- Challenge1_UnitTest/KomodoCafeTests.cs
+++ Challenge1_UnitTest/KomodoCafeTests.cs
@@ -1,6 +1,7 @@
 ﻿using Challenge1_Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Challenge1_UnitTest
 {
@@ -40,15 +41,32 @@ namespace Challenge1_UnitTest
         }
 
         [TestMethod]
-        public void GetMenuList()
+        public void GetMenuList_IsNotNull()
         {
-           
+            //Arrange
+            MenuRepositoryClass menuRepo = new MenuRepositoryClass();
+
+            //Act
+            List<MenuClass> menuItemsList = menuRepo.GetMenuList(); 
+
+            //Assert
+            Assert.IsNotNull(menuItemsList);
+        }
+
+        [TestMethod]
+        public void GetMenuItemsByMealName()
+        {
+            //Arrange
+            MenuRepositoryClass menuRepo = new MenuRepositoryClass();
+            MenuClass addedMenuItem = new MenuClass("1", "Burger Meal", "Burger w/ French Fries", "Beef patty, bun & cheese w/ french fries", 3.00);
+            menuRepo.CreateMenuItems(addedMenuItem);
+
+            //Act
+            MenuClass menuItemByName = menuRepo.GetMenuItemsByMealName(addedMenuItem.MealName);
+
+            //Assert
+            Assert.AreEqual(addedMenuItem, menuItemByName);
         }
     }
 }
 
-/*  //Read - List of all menu items
-public List<MenuClass> GetMenuList()
-{
-    return _menuItems;
-}*/
--- Challenge3_UnitTest/BadgesTests.cs
+++ Challenge3_UnitTest/BadgesTests.cs
@@ -7,32 +7,8 @@ namespace Challenge3_UnitTest
     [TestClass]
     public class BadgesTests
     {
-        /*[TestMethod]
-        public void TestMethod1()
-        {
-            BadgeClass badge = new BadgeClass();
-            badge = (123, new List<string>( "A1", "A2" ) );
-            BadgeRepository badgeRepo = new BadgeRepository();
-
-            badgeRepo.CreateNewBadge(badge);
-
-        }*/
+       
     }
 }
 
 
-/*[TestMethod]
-public void CreateMenuItems_ShouldNotGetNull()
-{
-    //Arrange
-    MenuClass menuItems = new MenuClass();
-    menuItems.MealName = "Cheeseburger";
-    MenuRepositoryClass menuRepo = new MenuRepositoryClass();
-
-    //Act
-    menuRepo.CreateMenuItems(menuItems);
-    MenuClass menuItemsFromMenu = menuRepo.GetMenuItemsByMealName("Cheeseburger");
-
-    //Assert
-    Assert.IsNotNull(menuItemsFromMenu);
-}*/
